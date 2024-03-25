d572a07ab050fd0544b5ee3d4a96d9719e1f5774
Erico <oliv.ericorib@gmail.com>
2024-03-25 20:00:21 -0300

    feat: dtos de resposta para update e add

---
+++ src/main/java/br/com/erico/tcc/sdp/dto/NovoProjetoResponseDto.java
@@ -0,0 +1,17 @@
+package br.com.erico.tcc.sdp.dto;
+
+import br.com.erico.tcc.sdp.enumeration.StatusEnum;
+import br.com.erico.tcc.sdp.model.Projeto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record NovoProjetoResponseDto(UUID id, String numero, String nome, String modalidade, String justificativa, UUID usuarioId, Integer eixoTecnologicoId, String impactosAmbientais,
+                                     LocalDate dataCricao, Short ano, StatusEnum status) {
+    public NovoProjetoResponseDto(Projeto p) {
+        this(p.getId(), p.getNumero(), p.getNome(), p.getModalidade(), p.getJustificativa(), p.getUsuario().getId(),
+                p.getEixoTecnologico().getId(), p.getImpactosAmbientais(), p.getDataCriacao(), p.getAno(),
+                StatusEnum.NAO_FINALIZADO);
+    }
+
+}
