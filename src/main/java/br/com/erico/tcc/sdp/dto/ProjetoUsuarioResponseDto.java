f0717103d2bd75300862d674e3b135fd722788a4
Erico <oliv.ericorib@gmail.com>
2024-03-27 10:42:30 -0300

    fix: remover atributo usuarioId

--- src/main/java/br/com/erico/tcc/sdp/dto/ProjetoUsuarioResponseDto.java
+++ src/main/java/br/com/erico/tcc/sdp/dto/ProjetoUsuarioResponseDto.java
@@ -3,5 +3,5 @@ package br.com.erico.tcc.sdp.dto;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record ProjetoUsuarioResponseDto(UUID id, UUID usuarioId, String numero, String nome, String modalidade, LocalDate dataCriacao, Short ano, Integer statusId, boolean finalizado, LocalDate dataFinalizacao) {
+public record ProjetoUsuarioResponseDto(UUID id, String numero, String nome, String modalidade, LocalDate dataCriacao, Short ano, Integer statusId, boolean finalizado, LocalDate dataFinalizacao) {
 }
