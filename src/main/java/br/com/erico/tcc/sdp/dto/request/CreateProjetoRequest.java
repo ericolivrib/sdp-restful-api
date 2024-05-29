5ac61f7b32dc307d78c1329011f1b29a024d94f7
Erico <oliv.ericorib@gmail.com>
2024-05-29 18:47:18 -0300

    fix: indentar DTO

--- src/main/java/br/com/erico/tcc/sdp/dto/request/CreateProjetoRequest.java
+++ src/main/java/br/com/erico/tcc/sdp/dto/request/CreateProjetoRequest.java
@@ -2,5 +2,13 @@ package br.com.erico.tcc.sdp.dto.request;
 
 import java.util.UUID;
 
-public record CreateProjetoRequest(String numero, String nome, String modalidade, String justificativa, UUID usuarioId, Integer eixoTecnologicoId, String impactosAmbientais) {
+public record CreateProjetoRequest(
+        String numero,
+        String nome,
+        String modalidade,
+        String justificativa,
+        UUID usuarioId,
+        Integer eixoTecnologicoId,
+        String impactosAmbientais
+) {
 }
